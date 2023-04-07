d3c7bb76ac7c7c0de0c9a61c449dda010f4d18b8
Siwon-Kim <siwonkim1108@gmail.com>
2023-04-07 02:24:02 -0400

    prob 13

---
+++ 2-algorithm/marathon/13.js
@@ -0,0 +1,7 @@
+function solution(a, b) {
+	const week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+	const day = new Date(`2016-${a}-${b}`).getDay();
+	return week[day];
+}
+
+// getDay() method
