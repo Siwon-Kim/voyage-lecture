d3eb01b1bebffbe667b0f3e2daf0e5ba9b10d872
Siwon-Kim <siwonkim1108@gmail.com>
2023-04-07 02:34:23 -0400

    prob 14

---
+++ 2-algorithm/marathon/14.js
@@ -0,0 +1,17 @@
+function solution(arr, divisor) {
+	var answer = [];
+	arr.forEach((e, i) => {
+		if (e % divisor === 0) {
+			answer.push(e);
+		}
+	});
+	if (answer.length === 0) {
+		answer.push(-1);
+	} else {
+		answer.sort((a, b) => {
+			return a - b;
+		});
+	}
+
+	return answer;
+}
