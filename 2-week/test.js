46ae66373effecc61b90d263cabb8467c97f3cc6
Siwon-Kim <siwonkim1108@gmail.com>
2023-04-12 02:02:54 -0400

    new Set()

--- 2-week/test.js
+++ 2-week/test.js
@@ -5,3 +5,6 @@ x > 0 && console.log("x is positive");
 let str3 = "Hello, World";
 console.log(str3.substr(7, 5)); // World (start_index, stop_index)
 console.log(str3.slice(7, 12)); // World (start_index, stop_index)
+
+const array = ['cat', 'dog', 'cat', 'puppy', 'kitten']
+console.log([...new Set(array)]);
