345e247f4914308b4f84d5fdd484c48d91323bd2
Siwon-Kim <siwonkim1108@gmail.com>
2023-04-15 02:45:33 -0400

    POST API

--- 3-week/SPA_MALL/app.js
+++ 3-week/SPA_MALL/app.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+
 const goodsRouter = require('./routes/goods.js')
+const connect = require('./schemas') // no need to add file names, it will be automatically imported
+connect();
+
+app.use(express.json()); // body로 전달받은 JSON 데이터는 바로 사용할 수 없어서 이를 jsonify해주어야 함. 따라서, 다른 app.use() middleware보다 상단에 있어야함
 
 app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
 
-app.use('/api', goodsRouter);
+app.use('/api', [goodsRouter]); 
+// goodsRouter is the array and [goodsRouter] will create an array with one each element
+// when you want to create a new array with a single element that is the value of a variable
 
 app.listen(port, () => {
 	console.log(port, "포트로 서버가 열렸어요!");
