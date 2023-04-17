6569536ac4ed27a7303f51defbee998756d65c2c
Siwon-Kim <siwonkim1108@gmail.com>
2023-04-17 07:02:17 -0400

    todo minimum

--- 3-week/TO_DO_MEMO/routes/todos.router.js
+++ 3-week/TO_DO_MEMO/routes/todos.router.js
@@ -25,27 +25,45 @@ router.get("/todos", async (req, res) => {
 	res.send({ todos });
 });
 
-router.patch('/todos/:todoId', async (req, res) => {
-    const {todoId} = req.params;
-    const {order} = req.body; // order가 되어야 하는 값이 반환된다
-    console.log(order);
-    // 1. todoId에 해당하는 할 일이 있는가?
-    // 1-1. 없으면 에러 출력
-    const currentTodo = await Todo.findById(todoId);
-    if(!currentTodo) {
-        return res.status(400).json({"errorMessage": "존재하지 않는 할 일 입니다."})
+router.patch("/todos/:todoId", async (req, res) => {
+	const { todoId } = req.params;
+	const { order, done, value } = req.body; // order가 되어야 하는 값이 반환된다
+
+	// 1. todoId에 해당하는 할 일이 있는가?
+	// 1-1. 없으면 에러 출력
+	const currentTodo = await Todo.findById(todoId);
+	if (!currentTodo) {
+		return res
+			.status(400)
+			.json({ errorMessage: "존재하지 않는 할 일 입니다." });
+	}
+
+	// 2. order가 존재하는가? - up, down 끝에 위치한 todos 예외 처리
+	if (order) {
+		const targetTodo = await Todo.findOne({ order }).exec();
+		if (targetTodo) {
+			targetTodo.order = currentTodo.order;
+			await targetTodo.save();
+		}
+		currentTodo.order = order;
+	} 
+    // 3. todo 내용이 변경되었는가?
+    else if(value) {
+        currentTodo.value = value;
+    } 
+    // 4. todo가 완료 되었는가?
+    else if(done !== undefined) {
+        currentTodo.doneAt = done ? new Date() : null; // true면 doneAt에 timestamp, false면 null처리
     }
 
-    // 2. order가 존재하는가? - up, down 끝에 위치한 todos 예외 처리
-    if(order) {
-        const targetTodo = await Todo.findOne({ order }).exec();
-        if(targetTodo) {
-            targetTodo.order = currentTodo.order;
-            await targetTodo.save();
-        }
-        currentTodo.order = order;
-        await currentTodo.save();
-    }
+    await currentTodo.save();
+
+	res.send({});
+});
+
+router.delete("/todos/:todoId", async (req, res) => {
+	const { todoId } = req.params;
+	await Todo.deleteOne({ _id: todoId });
 
     res.send({});
 });
